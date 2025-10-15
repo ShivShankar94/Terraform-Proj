7038d8467452658a7cd9acecac0bf3fe64fc4419
Shiv shankar Kumar <137617169+ShivShankar94@users.noreply.github.com>
2025-10-16 05:27:06 +0530

    Update main.tf

--- project-vpc-ec2/main.tf
+++ project-vpc-ec2/main.tf
@@ -4,14 +4,14 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "192.168.1.0/24"
+  cidr_block              = "192.167.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
   vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "192.168.2.0/24"
+  cidr_block              = "192.167.2.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
